d18e3adafcf1934a393b1fcea3304bb5208d9df1
unknown <richernote@gmail.com>
2022-08-03 20:52:22 -0700

    Continued with CRUD app tutorial
    added mongoDb database
    set env variables
    added ejs && views/index.ejs will delete index.html later
    changed post() to send to remote database
    changed get() to render ejs file && give ejs data
    in index.ejs put html from index.html and put in loop for data from database

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
+require('dotenv').config()
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({ extended: true }))
+MongoClient.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.nygvbno.mongodb.net/?retryWrites=true&w=majority`, { useUnifiedTopology: true })
+.then(client => {
+
+    console.log('Connected to DataBase')
+    const db = client.db('star-wars-quotes')
+    const quoteCollection = db.collection('quotes')
+
+        app.set('view engine', 'ejs')
+    
+        app.use(bodyParser.urlencoded({ extended: true }))
+        
+        app.post('/quotes', (req, res) => {
+            quoteCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(err => console.error(err))
+        })
+
+        app.get('/', (req, res) => {
+            const cursor = db.collection('quotes').find().toArray()
+                .then(results => {
+                    console.log(results)
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(err => console.error(err))
+
+        })
+
+        app.listen(3000, ()=> {
+            console.log('Listening on 3000')
+        })
+        
+    }).catch(error => console.error(error))
 
 
-app.listen(3000, ()=> {
-    console.log('Listening on 3000')
-})
-
-app.get('/', (req, res) => [
-    res.sendFile(__dirname + "/index.html")
-])
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
